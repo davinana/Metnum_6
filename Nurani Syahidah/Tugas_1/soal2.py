fc8dd069b6a851b878618c6025e66a301a269b5b
ransyaa <111947061+ransyaa@users.noreply.github.com>
2023-09-20 19:46:49 +0700

    Create soal2.py

---
+++ Nurani Syahidah/Tugas_1/soal2.py
@@ -0,0 +1,4 @@
+#NO.2
+#1. Buatlah modifikasi fungsi ketika kriteria program berhenti adalah sudah mencapai pada iterasi ke-n
+#2. Buatlah modifikasi agar user dapat menginputkan fungsi maupun batas akar dan galatnya
+#3. Buatlah modifikasi agar akarnya dapat ditampilkan dalam bentuk grafik
