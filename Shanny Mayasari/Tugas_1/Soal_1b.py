a6966723d5541b2330c65e62f51ff15791e74bf1
ShannyMaya <144967485+ShannyMaya@users.noreply.github.com>
2023-09-20 20:24:30 +0700

    Soal_1b.py

---
+++ Shanny Mayasari/Tugas_1/Soal_1b.py
@@ -0,0 +1,21 @@
+#No.1b.f(x) = e^x - x
+import numpy as np #untuk memanggil library panggil library
+
+#fungsi metode bagi dua
+def my_bisection(f, a, b, e):
+  if np.sign(f(a)) == np.sign(f(b)):
+    raise Exception('Tidak ada akar pada interval a dan b')
+  m = (a + b)/2
+  if np.abs(f(m)) < e:
+    return m
+  elif np.sign(f(a)) == np.sign(f(m)):
+    return my_bisection(f, m, b, e)
+  elif np.sign(f(b)) == np.sign(f(m)):
+    return my_bisection(f, a, m, e)
+
+#mendefinisikan fungsi f(x) = e^x - x
+
+f = lambda x: np.exp(x) - x #definisikan fungsi soal menggunakan lambda
+c = my_bisection(f, -2, 2, 0.01) #memanggil fungsi, dengan argumen yang diberikan
+print("c = ", c)
+print("f(c) = ", f(c))
